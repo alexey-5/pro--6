e8dec0d82d13d97ed1e7ee2a22073a9f3f6a2bdf
alexey-5 <107930630+alexey-5@users.noreply.github.com>
2022-11-08 20:51:27 +0300

    второй

--- src/App.js
+++ src/App.js
@@ -4,22 +4,51 @@ import "./index.scss";
 
 function App() {
   const [list, setList] = useState({});
+  const [inputL, setInputL] = useState(0);
+  const [inputR, setInputR] = useState(0);
+  const [sumbL, setSumbL] = useState("USD");
+  const [sumbR, setSumbR] = useState("RUB");
+  //console.log(inputL,inputR)
+ /*  useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then((json) => setUsers(json.data))
+      .catch((err)=>{console.log(err);
+      alert('ошибка при получении пользователей');
+      }).finally(()=>setIsLoading(false))
+  },[]); */
 
-  useEffect(() => {
-    fetch
-      .get("https://cdn.cur.su/api/latest.json")
+  useEffect(() => {// EUR GBP RUB
+    fetch("https://cdn.cur.su/api/latest.json")
       .then(res => res.json())
-      .then(res => setList(res))
+      .then(res => {setList(res.rates)
+      })
       .catch(err => console.log(err))
-  }, []);
+  },[]);
+
+  const inLeft = (v)=>{
+    setInputL(v)
+    setInputR(v*2)
+  }
+
+  const inRight = (v)=>{
+    setInputR(v)
+    setInputL(v*2)
+  }
+
   return (
     <div className="App">
       <Block
-        value={0}
-        currency="RUB"
-        onChangeCurrency={(cur) => console.log(cur)}
+        value={inputL}
+        currency={sumbL}
+        onChangeCurrency={(cur) => setSumbL(cur)}
+        onChangeValue={(v)=>inLeft(v)}
       />
-      <Block value={0} currency="USD" />
+      <Block value={inputR}
+       currency={sumbR} 
+       onChangeCurrency={(cur) => setSumbR(cur)}
+        onChangeValue={(v)=>inRight(v)}
+       />
     </div>
   );
 }
