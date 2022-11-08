17a84f589b636a83a802539e4ac34934e3dcf377
alexey-5 <107930630+alexey-5@users.noreply.github.com>
2022-11-08 18:07:09 +0300

    первый

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import { Block } from './Block';
-import './index.scss';
+import React, { useEffect, useState } from "react";
+import { Block } from "./Block";
+import "./index.scss";
 
 function App() {
+  const [list, setList] = useState({});
+
+  useEffect(() => {
+    fetch
+      .get("https://cdn.cur.su/api/latest.json")
+      .then(res => res.json())
+      .then(res => setList(res))
+      .catch(err => console.log(err))
+  }, []);
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
+      <Block
+        value={0}
+        currency="RUB"
+        onChangeCurrency={(cur) => console.log(cur)}
+      />
       <Block value={0} currency="USD" />
     </div>
   );
