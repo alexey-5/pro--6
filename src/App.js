d364c26849fac6f129a2abde709314c1a9e1d2d7
alexey-5 <107930630+alexey-5@users.noreply.github.com>
2022-11-09 16:49:12 +0300

    финальный вариант

--- src/App.js
+++ src/App.js
@@ -8,32 +8,38 @@ function App() {
   const [inputR, setInputR] = useState(0);
   const [sumbL, setSumbL] = useState("USD");
   const [sumbR, setSumbR] = useState("RUB");
-  //console.log(inputL,inputR)
- /*  useEffect(() => {
-    fetch("https://reqres.in/api/users")
-      .then((res) => res.json())
-      .then((json) => setUsers(json.data))
-      .catch((err)=>{console.log(err);
-      alert('ошибка при получении пользователей');
-      }).finally(()=>setIsLoading(false))
-  },[]); */
-
-  useEffect(() => {// EUR GBP RUB
+  //console.log(sumbL,list[sumbR])
+  useEffect(() => {
+    // EUR GBP RUB
     fetch("https://cdn.cur.su/api/latest.json")
-      .then(res => res.json())
-      .then(res => {setList(res.rates)
+      .then((res) => res.json())
+      .then((res) => {
+        setList(res.rates);
       })
-      .catch(err => console.log(err))
-  },[]);
+      .catch((err) => console.log(err));
+  }, []);
 
-  const inLeft = (v)=>{
-    setInputL(v)
-    setInputR(v*2)
+  const kf = (s1, s2) => {
+    return (list[s2]/list[s1]);
+  };
+// ====  Пересчёт при вводе input
+  const inLeft = (v) => {
+    setInputL(v);
+    setInputR(v * kf(sumbL, sumbR));
+  };
+  const inRight = (v) => {
+    setInputR(v);
+    setInputL(v * kf(sumbR, sumbL));
+  };
+// ====  Пересчёт при смене валюты
+  const changeForexL = (cur)=>{
+    setSumbL(cur)
+    //console.log(sumbL)
+    setInputL(inputR * kf(sumbR, cur))
   }
-
-  const inRight = (v)=>{
-    setInputR(v)
-    setInputL(v*2)
+  const changeForexR = (cur)=>{
+    setSumbR(cur)
+    setInputR(inputL * kf(sumbL, cur))
   }
 
   return (
@@ -41,16 +47,16 @@ function App() {
       <Block
         value={inputL}
         currency={sumbL}
-        onChangeCurrency={(cur) => setSumbL(cur)}
-        onChangeValue={(v)=>inLeft(v)}
+        onChangeCurrency={(cur) => changeForexL(cur)}
+        onChangeValue={(v) => inLeft(v)}
       />
-      <Block value={inputR}
-       currency={sumbR} 
-       onChangeCurrency={(cur) => setSumbR(cur)}
-        onChangeValue={(v)=>inRight(v)}
-       />
+      <Block
+        value={inputR}
+        currency={sumbR}
+        onChangeCurrency={(cur) => changeForexR(cur)}
+        onChangeValue={(v) => inRight(v)}
+      />
     </div>
   );
 }
-
 export default App;
